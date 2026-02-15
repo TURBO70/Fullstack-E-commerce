46b6d3ad1735dcae983b5f94daefd6ef66013abd
hanna-adel <adelhana895@gmail.com>
2026-02-15 23:21:29 +0200

    make userid in orders synced with userid in user

--- Frontend/e-commerce/src/app/core/services/order_service.ts
+++ Frontend/e-commerce/src/app/core/services/order_service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Order } from '../../shared/models/order_model';
+import { AuthService } from './auth-service';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -8,7 +10,7 @@ import { Order } from '../../shared/models/order_model';
 export class OrderService {
 
   private BaseUrl = 'http://localhost:3000/orders';
-  constructor(private http:HttpClient) {}
+  constructor(private http:HttpClient,private authService: AuthService) {}
   
   getAllOrders() {
     return this.http.get<Order[]>(this.BaseUrl);
@@ -17,14 +19,22 @@ export class OrderService {
     return this.http.get<Order[]>(`${this.BaseUrl}?userId=${userId}`);
   }
 
-  createOrder(order: Omit<Order, 'id'>) {
+ // Create order - automatically adds userId from logged-in user
+ createOrder(orderData: Omit<Order, 'id' | 'userId' | 'createdAt'>): Observable<Order> {
+    const currentUser = this.authService.getCurrentUser();
+    const order: Omit<Order, 'id'> = {
+      ...orderData,
+      userId: currentUser.id, 
+      createdAt: new Date().toISOString().split('T')[0], 
+    };
     return this.http.post<Order>(this.BaseUrl, order);
   }
 
-  updateOrderStatus(orderId: string, status: 'pending' | 'confirmed' | 'shipped') {
+  updateOrderStatus(orderId: string, status: 'pending' | 'confirmed' | 'shipped'):Observable<Order> {
     return this.http.patch<Order>(`${this.BaseUrl}/${orderId}`, { status });
   }
 
-
-  
+  deleteOrder(orderId: string): Observable<void> {
+    return this.http.delete<void>(`${this.BaseUrl}/${orderId}`);
+  }
 }
